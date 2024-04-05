b64c78743402cb2839aa47044876bfdb47739f33
srinu2003 <tsrin2003@gmail.com>
2024-04-06 02:23:56 +0530

    Refactor code and add Lab-6 files

--- MergeSort.php
+++ MergeSort.php
@@ -8,12 +8,12 @@
 <body>
     Descending order.
     <?php
-    echo "<br><br>";
+    echo "<br>";
     $a1 = array(3, 5, 13, 7, 6);
     $a2 = array(4, 3, 20, 1, 6);
     $num = array_merge($a1, $a2);
     array_multisort($num, SORT_DESC, SORT_NUMERIC);
     print_r($num);
-    ?>
+?>
 </body>
 </html>
